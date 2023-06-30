d09b39ca792748f42c117b8af63512465be52542
yehuda <yehuda.mescheloff@mail.huji.ac.il>
2023-06-30 16:45:16 +0300

    dynamics: only trying to switch only values with different values, more jitted functions
    main:
    gif creation canceled, added clusters number check (only for the end of each run)

--- dynamics.py
+++ dynamics.py
@@ -1,10 +1,21 @@
 import numpy as np
-from numba import jit
+from numba import jit, njit
 
 import constants
+import initialize
 
 
-def kawasaki_time_step(matrix, J, temperature):
+
+@njit
+def kawasaki_in_jit_mode(matrix, T, neighbors_matrix, different_neighbor_numbers, steps_num: int, exponent_dict):
+    for i in range(steps_num):
+        matrix, neighbors_matrix, different_neighbor_numbers = kawasaki_time_step(matrix, T, neighbors_matrix,
+                                                                                           different_neighbor_numbers, exponent_dict)
+    return matrix, neighbors_matrix, different_neighbor_numbers
+
+
+@njit
+def kawasaki_time_step(matrix, temperature, neighbors_matrix, different_neighbor_numbers):
     """
     preforms one kawasaki time step:
         pick random particle A
@@ -19,55 +30,57 @@ def kawasaki_time_step(matrix, J, temperature):
 
     :param matrix:
     :param temperature:
-    :return: updated matrix
+    :return: updated cluster_matrix
     """
     N = matrix.shape[0]
-    time = 0
-    while True:  # continue until a step is made
+    for time in range(constants.TIMEOUT):  # continue until a step is made
         # timeout logic for not finding any swap
-        time += 1
-        if time >= constants.TIMEOUT:
-            print("timeout")
-            return matrix
 
-        i, j, neighbor_i, neighbor_j = get_site_and_neighbor(N, matrix)
-        if (matrix[i, j] == matrix[neighbor_i, neighbor_j]):
-            continue
+        i, j, neighbor_i, neighbor_j = get_site_and_neighbor(N, matrix, neighbors_matrix, different_neighbor_numbers)
+        # if (cluster_matrix[i, j] == cluster_matrix[neighbor_i, neighbor_j]):
+        #     continue
 
         # calculate E, swap and calculate E'
-        # E = calculate_energy(matrix, J)
-        # matrix[i, j], matrix[neighbor_i, neighbor_j] = matrix[neighbor_i, neighbor_j], matrix[
+        # E = calculate_energy(cluster_matrix, J)
+        # cluster_matrix[i, j], cluster_matrix[neighbor_i, neighbor_j] = cluster_matrix[neighbor_i, neighbor_j], cluster_matrix[
         # 	i, j]
-        # E_prime = calculate_energy(matrix, J)
+        # E_prime = calculate_energy(cluster_matrix, J)
 
-        delta_E = calculate_energy_diff(matrix, J, (i, j), (neighbor_i, neighbor_j))
+        delta_E = calculate_energy_diff(matrix, (i, j), (neighbor_i, neighbor_j), different_neighbor_numbers)
         # might need to change probability calculation
         if delta_E <= 0:
-            # matrix = swap_matrix
-            matrix = swap_points(matrix, (i, j), (neighbor_i, neighbor_j))
+            # cluster_matrix = swap_matrix
+            matrix, neighbors_matrix, different_neighbor_numbers = swap_points(matrix, (i, j), (neighbor_i, neighbor_j),
+                                                                               neighbors_matrix,
+                                                                               different_neighbor_numbers)
 
             break
         else:
             probability = np.exp(-delta_E / temperature)
+            # probability=exponents_dict[delta_E]
             if np.random.rand() < probability:
-                # matrix = swap_matrix
-                matrix = swap_points(matrix, (i, j), (neighbor_i, neighbor_j))
+                # cluster_matrix = swap_matrix
+                matrix, neighbors_matrix, different_neighbor_numbers = swap_points(matrix, (i, j),
+                                                                                   (neighbor_i, neighbor_j),
+                                                                                   neighbors_matrix,
+                                                                                   different_neighbor_numbers)
                 break
-            # matrix[i, j], matrix[neighbor_i, neighbor_j] = matrix[neighbor_i, neighbor_j], matrix[
+            # cluster_matrix[i, j], cluster_matrix[neighbor_i, neighbor_j] = cluster_matrix[neighbor_i, neighbor_j], cluster_matrix[
             # 	i, j]
             continue
-    return matrix
+    return matrix, neighbors_matrix, different_neighbor_numbers
 
 
-def get_site_and_neighbor(N, matrix):
+@njit
+def get_site_and_neighbor(N, matrix, neighbors_matrix, different_neighbor_numbers):
     # because we want to switch between values with different value, choosing
     # only from one type is the same
-    minority_places=np.where(matrix==constants.MINORITY_VALUE)
-    ind = np.random.randint(len(minority_places[0]))
-    i,j = minority_places[0][ind], minority_places[1][ind]
-    # i, j = np.random.randint(0, N, 2)  # todo: change to choice for ==1
-    neighbor = constants.NEIGHBOR[
-        np.random.randint(0, 4)]  # problem with probability of picking an edge - where do you account for that?
+    minority_places = np.where((matrix == constants.MINORITY_VALUE) & (different_neighbor_numbers > 0))
+    ind = np.random.randint(0, len(minority_places[0]))
+    i, j = minority_places[0][ind], minority_places[1][ind]
+    # i, j = np.random.randint(0, N, 2)
+    neighbor = constants.NEIGHBOR[neighbors_matrix[i, j]][np.random.randint(0, different_neighbor_numbers[
+        i, j])]  # problem with probability of picking an edge - where do you account for that?
     neighbor_i = (i + neighbor[0]) % N  # currently we do periodic edges
     neighbor_j = (j + neighbor[1]) % N
     return i, j, neighbor_i, neighbor_j
@@ -76,70 +89,89 @@ def get_site_and_neighbor(N, matrix):
 def increment_density(matrix, higher_density):
     """
     how to update the density? pick a 0 site at random and flip it to 1?
-    :return: changed matrix
+    :return: changed cluster_matrix, new neighbors cluster_matrix
     """
     N_spins = np.prod(matrix.shape)
     original_density = np.sum(matrix == constants.MINORITY_VALUE) / N_spins
-    if original_density>=higher_density:
-        return matrix
+    if original_density >= higher_density:
+        return matrix, initialize.create_neighbors_matrix(matrix)
     spins_to_add = int(N_spins * (higher_density - original_density))
     majority_indexes = np.where(matrix == constants.MAJORITY_VALUE)
     change_indexes = np.random.choice(np.array(majority_indexes[0].shape[0], dtype=int), spins_to_add, replace=False)
     matrix[
         tuple((dimension_indexes[change_indexes] for dimension_indexes in majority_indexes))] = constants.MINORITY_VALUE
-    return matrix
+    return matrix, initialize.create_neighbors_matrix(matrix)
 
 
-@jit()
-def calculate_energy_diff(matrix, J, point_A, point_B):
+@njit
+def calculate_energy_diff(matrix, point_A, point_B, different_neighbor_numbers):
     """
+    calculate the energy difference for switching pointA and B, assuming they are nearest neighbors
     :param matrix:
     :param point_A:
     :param point_B:
     :return: energy difference from swapping the two points
     """
+    i, j = point_A
+    neighbor_i, neighbor_j = point_B
+    return 2 * constants.CONSTANT_J * (
+            4 * constants.DIMENSION + 2 - 2 * (different_neighbor_numbers[i, j] + different_neighbor_numbers[
+        neighbor_i, neighbor_j]))
+    # delta E=-2 * original E, couple E=-J*(4d+2-2*(diff_num(A)+diff_num(B)))
+    # before = calc_energy_point(cluster_matrix, point_A) + calc_energy_point(cluster_matrix, point_B)
+    # cluster_matrix = swap_points(cluster_matrix, point_A, point_B)
+    # after = calc_energy_point(cluster_matrix, point_A) + calc_energy_point(cluster_matrix, point_B)
+    # cluster_matrix = swap_points(cluster_matrix, point_A, point_B)  # needed!!!
+    # return after - before
 
-    before = calc_energy_point(matrix, J, point_A) + calc_energy_point(matrix, J, point_B)
-    matrix = swap_points(matrix, point_A, point_B)
-    after = calc_energy_point(matrix, J, point_A) + calc_energy_point(matrix, J, point_B)
-    matrix = swap_points(matrix, point_A, point_B)  # needed!!!
-    return after - before
 
-
-@jit()
-def swap_points(matrix, point_A, point_B):
+@njit
+def swap_points(matrix, point_A, point_B, neighbors_matrix, different_neighbor_numbers):
     i, j = point_A
     neighbor_i, neighbor_j = point_B
     matrix[i, j], matrix[neighbor_i, neighbor_j] = matrix[neighbor_i, neighbor_j], matrix[
         i, j]
-    return matrix
+    for neighbor_num, neighbor in enumerate(constants.NEIGHBOR):
+        # original neighbor is the neighbor of i,j
+        original_neighbor_i = (i - neighbor[0]) % constants.MATRIX_SIZE
+        original_neighbor_j = (j - neighbor[1]) % constants.MATRIX_SIZE
+        neighbors_matrix[original_neighbor_i, original_neighbor_j, neighbor_num] = ~neighbors_matrix[
+            original_neighbor_i, original_neighbor_j, neighbor_num]
+        different_neighbor_numbers[original_neighbor_i, original_neighbor_j] = np.sum(
+            neighbors_matrix[original_neighbor_i, original_neighbor_j])
+
+        neighbor_neighbor_i = (neighbor_i - neighbor[0]) % constants.MATRIX_SIZE
+        neighbor_neighbor_j = (neighbor_j - neighbor[1]) % constants.MATRIX_SIZE
+        neighbors_matrix[neighbor_neighbor_i, neighbor_neighbor_j, neighbor_num] = ~neighbors_matrix[
+            neighbor_neighbor_i, neighbor_neighbor_j, neighbor_num]
+        different_neighbor_numbers[neighbor_neighbor_i, neighbor_neighbor_j] = np.sum(
+            neighbors_matrix[neighbor_neighbor_i, neighbor_neighbor_j])
+
+    neighbors_matrix[i, j] = ~neighbors_matrix[i, j]
+    different_neighbor_numbers[i, j] = np.sum(neighbors_matrix[i, j])
+    neighbors_matrix[neighbor_i, neighbor_j] = ~neighbors_matrix[neighbor_i, neighbor_j]
+    different_neighbor_numbers[neighbor_i, neighbor_j] = np.sum(neighbors_matrix[neighbor_i, neighbor_j])
+    return matrix, neighbors_matrix, different_neighbor_numbers
 
 
-@jit()
-def calc_energy_point(matrix, J, point):
+@njit
+def calc_energy_point(matrix, point, different_neighbor_numbers):
     i, j = point
     N = matrix.shape[0]
-    return -matrix[i, j] * (
-            matrix[(i + 1) % N, j] * J[constants.VERTICAL, i, j]
-            + matrix[(i - 1) % N, j] * J[constants.VERTICAL, (i - 1) % N, j]
-            + matrix[i, (j + 1) % N] * J[constants.HORIZONTAL, i, j]
-            + matrix[i, (j - 1) % N] * J[constants.HORIZONTAL, i, (j - 1) % N])
+    return -constants.CONSTANT_J * (2 * len(N) - 2 * different_neighbor_numbers[i, j])
 
 
-@jit()
-def calculate_energy(matrix, J):
+@njit
+def calculate_energy(matrix, different_neighbor_numbers):
     """
     calculate the energy of the system
     :param matrix:
     :return:
     """
-    energy = 0
-    N = matrix.shape[0]
-    for i in range(N):
-        for j in range(N):
-            energy -= matrix[i, j] * (
-                    matrix[(i + 1) % N, j] * J[constants.VERTICAL, i, j]
-                    + matrix[(i - 1) % N, j] * J[constants.VERTICAL, (i - 1) % N, j]
-                    + matrix[i, (j + 1) % N] * J[constants.HORIZONTAL, i, j]
-                    + matrix[i, (j - 1) % N] * J[constants.HORIZONTAL, i, (j - 1) % N])
-    return energy / 2
+    return -constants.CONSTANT_J * (
+            constants.DIMENSION * constants.N_SPINS - np.sum(different_neighbor_numbers))
+    # energy = -np.sum(cluster_matrix[1:] * cluster_matrix[:-1])
+    # energy -= np.sum(cluster_matrix[0] * cluster_matrix[-1])
+    # energy -= np.sum(cluster_matrix[:, 1:] * cluster_matrix[:, :-1])
+    # energy -= np.sum(cluster_matrix[:, 0] * cluster_matrix[:, -1])
+    # return constants.CONSTANT_J * energy
--- main.py
+++ main.py
@@ -1,58 +1,95 @@
-import constants
-import initialize
-import dynamics
-import numpy as np
+import os.path
+
 import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
+import clusters
+import constants
+import dynamics
+import initialize
 
-def run_with_densities(N=constants.MATRIX_SIZE, densities=[constants.INITIAL_ZERO_DENSITY], T=constants.INITIAL_TEMPERATURE,
-		 J=constants.CONSTANT_J, steps=constants.NUMBER_OF_STEPS, gif_name="visual/vis.gif"):
-	matrix = initialize.initialize_matrix(N, densities[0])
-	densities_number=len(densities)
-	steps_per_density=steps//densities_number
-	energy = []
-	imgs = []
 
-	for i in tqdm(range(steps_per_density*densities_number-1)):
-		matrix = dynamics.kawasaki_time_step(matrix, J, T)
-		if (i*constants.NUMBER_OF_FRAMES) % steps == 0:
-			energy.append(dynamics.calculate_energy(matrix, J))
-			imgs.append(Image.fromarray((matrix==constants.MINORITY_VALUE) * 127.5))
-		if i%steps_per_density == 0:
-			if i==0:
-				continue
-			matrix = dynamics.increment_density(matrix, densities[i//steps_per_density])
+def run_with_densities(N: int = constants.MATRIX_SIZE, densities = [constants.INITIAL_ZERO_DENSITY],
+                       T: float = constants.INITIAL_TEMPERATURE, steps: int = constants.NUMBER_OF_STEPS,
+                       gif_name: str = "visual/vis.gif"):
+    matrix = initialize.initialize_matrix(N, densities[0])
+    neighbors_matrix = initialize.create_neighbors_matrix(matrix)
+    different_neighbor_numbers = np.sum(neighbors_matrix, axis=len(neighbors_matrix.shape) - 1)
+    densities_number = len(densities)
+    steps_per_density = steps // densities_number
+    # energy = []
+    # imgs = []
 
-	# print(int(constants.GIF_LENGTH*10**3/len(imgs)))
-	# print(len(imgs))
-	imgs[0].save(gif_name, save_all=True, append_images=imgs[1:], duration=1,loop=1) #
+    for i in tqdm(range(steps_per_density * (densities_number) - 1)):
+        matrix, neighbors_matrix, different_neighbor_numbers = dynamics.kawasaki_time_step(matrix, T, neighbors_matrix,
+                                                                                           different_neighbor_numbers)
+        # if (i * constants.NUMBER_OF_FRAMES) % steps == 0:
+        #     energy.append(dynamics.calculate_energy(matrix, different_neighbor_numbers))
+        #     imgs.append(Image.fromarray((matrix == constants.MINORITY_VALUE) * 255))
+        if i % steps_per_density == 0:
+            if i == 0:
+                continue
+            matrix, neighbors_matrix = dynamics.increment_density(matrix, densities[i // steps_per_density])
+            different_neighbor_numbers = np.sum(neighbors_matrix, axis=len(neighbors_matrix.shape) - 1)
+    matrix, neighbors_matrix, different_neighbor_numbers = dynamics.kawasaki_time_step(matrix, T, neighbors_matrix,
+                                                                                       different_neighbor_numbers)
+    # energy.append(dynamics.calculate_energy(matrix, different_neighbor_numbers))
+    # imgs.append(Image.fromarray((matrix == constants.MINORITY_VALUE) * 255))
+    # imgs[0].save(gif_name, save_all=True, append_images=imgs[1:], duration=1, loop=1)  #
+    return matrix
 
-	plt.plot(energy)
-	plt.show()
 
-def main(N=constants.MATRIX_SIZE, density=constants.INITIAL_ZERO_DENSITY, T=constants.INITIAL_TEMPERATURE,
-		 J=constants.CONSTANT_J, steps=constants.NUMBER_OF_STEPS, gif_name="visual/vis.gif"):
-	matrix = initialize.initialize_matrix(N, density)
 
-	energy = []
-	imgs = []
-	for i in tqdm(range(steps)):
-		matrix = dynamics.kawasaki_time_step(matrix, J, T)
-		if (i*constants.NUMBER_OF_FRAMES) % steps == 0:
-			energy.append(dynamics.calculate_energy(matrix, J))
-			imgs.append(Image.fromarray((matrix + 1) * 127.5))
 
-	# print(int(constants.GIF_LENGTH*10**3/len(imgs)))
-	# print(len(imgs))
-	imgs[0].save(gif_name, save_all=True, append_images=imgs[1:], duration=1,loop=1) #
 
-	plt.plot(energy)
-	plt.show()
+def check_values(initial_den, final_den, total_change_steps, steps_at_final_den, times_to_repeat):
+    dens = np.concatenate([np.linspace(initial_den, final_den, total_change_steps), [final_den] * steps_at_final_den])
+    clusters_num = []
+    for i in range(times_to_repeat):
+        matrix = run_with_densities(T=0.7, densities=dens, gif_name="visual/densities.gif")
+        clusters_num.append(clusters.get_clusters_number(matrix, constants.MINORITY_VALUE, 8))
+    return np.mean(clusters_num), np.std(clusters_num)
+
+
+def main(final_den, rep_num):
+    mean_path=f"npy_files/mean_clusters_num_{final_den}_{constants.NUMBER_OF_STEPS}_steps rep_num_{rep_num}"
+    std_path=f"npy_files/clusters_std_map_{final_den}_{constants.NUMBER_OF_STEPS}_steps rep_num_{rep_num}"
+    initial_den=3/64**2
+    changes_steps_nums_list=[int(((64**2-3)/4)//i) for i in range(1,10)] + list(np.linspace(65, 0, 5, dtype=int))
+    steps_at_final_density= [1,2,5,10,15,20,25,30]
+    if os.path.exists(mean_path):
+        mean_cluster_num_map=np.load(mean_path)
+    else:
+        mean_cluster_num_map=np.zeros((len(steps_at_final_density),len(changes_steps_nums_list)))
+    if os.path.exists(std_path):
+        cluster_std_map=np.load(std_path)
+    else:
+        cluster_std_map=np.zeros((len(steps_at_final_density),len(changes_steps_nums_list)))
+    try:
+        for i,steps_at_final_den in enumerate(steps_at_final_density):
+            for j, total_changes_steps_num in enumerate(changes_steps_nums_list):
+                if mean_cluster_num_map[i,j]==0:
+                    print("i, j=",i,j)
+                    mean_cluster_num_map[i, j],cluster_std_map[i,j] = check_values(initial_den, final_den,
+                                                                               total_changes_steps_num,
+                                                                               steps_at_final_den, rep_num)
+    finally:
+        np.save(mean_path, mean_cluster_num_map)
+        np.save(std_path, cluster_std_map)
+        print(f"{final_den} density heat map:")
+        print(mean_cluster_num_map)
+        print("std map:")
+        print(cluster_std_map, end="\n\n")
+
+
 
 if __name__ == '__main__':
-	run_with_densities(T=0.7,densities=np.concatenate([np.arange(2,64,1)/64**2, np.ones(5)/64,np.linspace(3/128, 0.5, 15), 0.5*np.ones(3)]), gif_name="visual/densities.gif")
-	# main(T=0.1)
-	# for t in np.linspace(1e-3, 2,15):
-	# 	main(T=t, gif_name=f"visual/T={round(t,3)}.gif")
+    np.random.seed(1)
+    main(0.1,5)
+    main(0.25,5)
+    # run_with_densities(N=constants.MATRIX_SIZE)
+# main(T=0.1)
+# for t in np.linspace(1e-3, 2,15):
+# 	main(T=t, gif_name=f"visual/T={round(t,3)}.gif")
